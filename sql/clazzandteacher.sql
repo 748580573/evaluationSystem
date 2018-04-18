714b6d9647d3cdd70a0c5ad847117a0452fa79ea
748580573 <748580573@qq.com>
2018-04-18 16:33:46 +0800

    修改了sql文件

---
+++ sql/clazzandteacher.sql
@@ -0,0 +1,4 @@
+INSERT INTO `clazzandteacher` VALUES (1, '03', '123654888');
+INSERT INTO `clazzandteacher` VALUES (2, '04', '123654888');
+INSERT INTO `clazzandteacher` VALUES (3, '04', '2018031701');
+INSERT INTO `clazzandteacher` VALUES (4, '04', '2018031603');
